bec5f79c36944d877175c285ca5dd0a88e2c0f7c
ahadvu436-maker <ahadvu436@gmail.com>
2026-09-05 08:01:44 +0600

    Add Footer component with copyright information

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <footer className="footer" id="footer">
+      <p>&copy; 2026 Mousum Graphics. All rights reserved.</p>
+    </footer>
+  );
+}
